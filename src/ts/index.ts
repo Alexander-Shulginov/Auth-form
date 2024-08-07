a18341a12733f497f77607c1fe89c91780fd924f
Alexander Shulginov <alexander3post@gmail.com>
2024-08-07 21:46:40 +0300

    add service animation

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -7,3 +7,14 @@ import togglePasswordVisibility from './ui/TogglePasswordVisibility';
 initTabs();
 togglePlaceholder();
 // togglePasswordVisibility();
+
+// const el: NodeList = document.querySelectorAll('.service__elem');
+
+// el.forEach((item: HTMLElement) => {
+//     item.addEventListener('click', () => {
+//         item.classList.add('service__elem--clicked');
+//         setTimeout(() => {
+//             item.classList.remove('service__elem--clicked');
+//         }, 400);
+//     });
+// });
